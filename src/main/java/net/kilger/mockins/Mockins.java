40845ad5efe5d48414c347f1de21db6e4778341b
kilgerm <mk1@m-kilger.de>
2012-07-23 23:06:02 +0200

    improved readme and Mockins javadoc

--- src/main/java/net/kilger/mockins/Mockins.java
+++ src/main/java/net/kilger/mockins/Mockins.java
@@ -5,7 +5,22 @@ import net.kilger.mockins.generator.TestCallInterceptor;
 import net.sf.cglib.proxy.Enhancer;
 
 /**
- * This is the base API to use Mockins.
+ * <p>This is the base API class to use Mockins.</p>
+ * <p>
+ * Example:
+ * <pre>
+ * // This is the class you would like to test
+ * MyClass classUnderTest = new MyClass();
+ * 
+ * // first, create an "instructed" wrapper
+ * instructed = Mockins.instruct(classUnderTest);
+ * 
+ * // then call whatever method you would like to test...
+ * instructed.testMethod(null);
+ * 
+ * // ...this will run testMethod and try to add mocks/stubs for parameters and fields so that the method does not throw a NPE!
+ * </pre>
+ * <p>
  */
 public final class Mockins {
 
